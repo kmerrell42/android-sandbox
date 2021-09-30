a837a4f992df699b15fffcfa664a0b03c2e646e8
Kelly Merrell <kmerrell@mercuryintermedia.com>
2021-09-30 18:04:47 -0500

    Converting view to Compose

--- app/src/main/java/io/mercury/coroutinesandbox/view/main/MainActivity.kt
+++ app/src/main/java/io/mercury/coroutinesandbox/view/main/MainActivity.kt
@@ -1,14 +1,26 @@
 package io.mercury.coroutinesandbox.view.main
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
+import com.google.accompanist.appcompattheme.AppCompatTheme
 import dagger.hilt.android.AndroidEntryPoint
 import io.mercury.coroutinesandbox.R.string
-import io.mercury.coroutinesandbox.databinding.ActivityMainBinding
 import io.mercury.coroutinesandbox.view.main.MainFeature.Action
 import io.mercury.coroutinesandbox.view.main.MainFeature.Action.Cancel
 import io.mercury.coroutinesandbox.view.main.MainFeature.Action.Download
@@ -21,70 +33,43 @@ import io.mercury.coroutinesandbox.view.main.MainFeature.State.Downloading
 import io.mercury.coroutinesandbox.view.main.MainFeature.State.Unloaded
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : ComponentActivity() {
 
     private val actionPublisher = MutableSharedFlow<Action>(replay = 0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
         val model: MainViewModel by viewModels()
 
         lifecycleScope.launchWhenCreated { actionPublisher.collect(model.feature::onAction) }
-        lifecycleScope.launchWhenCreated { model.feature.state.collect(::onNewState) }
+        lifecycleScope.launchWhenCreated { model.feature.state.collect(::renderScreen) }
         lifecycleScope.launchWhenCreated { model.feature.newsEvent.collect(::onNewsBroadcast) }
     }
 
-    // Functional: Should we bind functions by state to get the when clause out of our face?
-    // Maybe the single accept(...) helps drive the single state idea?
-    private fun onNewState(state: State) {
-        when(state) {
-            is Unloaded -> {
-                binding.loadBtn.apply {
-                    text = getString(string.download_update)
-                    setOnClickListener { publishAction(Download) }
+    private fun renderScreen(state: State) {
+        setContent {
+            when (state) {
+                is Unloaded -> {
+                    MainScreen(msg = getString(string.update_available),
+                        btnLabel = getString(string.download_update),
+                        btnAction = { publishAction(Download) })
                 }
-
-                binding.content.visibility = View.GONE
-                binding.loading.apply {
-                    visibility = View.GONE
-                    text = getString(string.update_available)
+                is Downloading -> {
+                    MainScreen(msg = getString(
+                        string.download_percent_format,
+                        state.percentCompete
+                    ),
+                        btnLabel = getString(string.cancel),
+                        btnAction = { publishAction(Cancel) })
                 }
-            }
-            is Downloading -> {
-                binding.loadBtn.apply {
-                    text = getString(string.cancel)
-                    setOnClickListener { publishAction(Cancel) }
-                }
-                binding.content.visibility = View.GONE
-                binding.loading.apply {
-                    visibility = View.VISIBLE
-                    text = getString(string.download_percent_format, state.percentCompete)
-                }
-            }
-            is Downloaded -> {
-                binding.loadBtn.apply {
-                    text = getString(string.unload)
-                    setOnClickListener { publishAction(Unload)}
-                }
-
-                binding.loading.visibility = View.GONE
-
-                binding.content.apply {
-                    visibility = View.VISIBLE
-                    text = getString(string.download_complete)
+                is Downloaded -> {
+                    MainScreen(msg = getString(string.download_complete),
+                        btnLabel = getString(string.unload),
+                        btnAction = { publishAction(Unload) })
                 }
             }
         }
@@ -102,5 +87,35 @@ class MainActivity : AppCompatActivity() {
         lifecycleScope.launch {
             actionPublisher.emit(action)
         }
+    }
+
+    @Composable
+    fun MainScreen(msg: String, btnLabel: String, btnAction: () -> Unit) {
+        AppCompatTheme {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.colors.background)
+            ) {
+                Text(
+                    text = msg,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.align(Companion.Center)
+                )
+
+                Button(onClick = btnAction,
+                    modifier = Modifier
+                        .align(Companion.BottomCenter)
+                        .offset(0.dp, (-24).dp)) {
+                    Text(text = btnLabel)
+                }
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMainScreen() {
+        MainScreen(msg = "Message", btnLabel = "Button Label") {}
     }
 }
