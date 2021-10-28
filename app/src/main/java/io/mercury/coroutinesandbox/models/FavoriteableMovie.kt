54164346f4a4a5108c0d49f539836364bb5c3a44
Kelly Merrell <kmerrell@mercuryintermedia.com>
2021-10-28 10:04:42 -0500

    Adding a download indicator to the MovieCard

--- app/src/main/java/io/mercury/coroutinesandbox/models/FavoriteableMovie.kt
+++ app/src/main/java/io/mercury/coroutinesandbox/models/FavoriteableMovie.kt
@@ -1,8 +1,11 @@
 package io.mercury.coroutinesandbox.models
 
+import io.mercury.coroutinesandbox.interactors.GetDownloadedStatuses.DownloadState
+
 data class FavoriteableMovie(
     val id: String,
     val title: String,
     val posterUrl: String,
-    val isFavorite: Boolean
+    val isFavorite: Boolean,
+    val downloadState: DownloadState
 )
