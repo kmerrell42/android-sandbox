763a586a1eb55e5f9928bb4cd6707cb0704ca6b9
Kelly Merrell <kmerrell@mercuryintermedia.com>
2021-10-29 10:18:34 -0500

    Making MovieCardFactory an actual factory no just a dependency holder

--- app/src/main/java/io/mercury/coroutinesandbox/view/component/MoviesListComponents.kt
+++ app/src/main/java/io/mercury/coroutinesandbox/view/component/MoviesListComponents.kt
@@ -1,27 +1,14 @@
 package io.mercury.coroutinesandbox.view.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
-import coil.size.Scale
-import dagger.hilt.android.scopes.ActivityScoped
 import io.mercury.coroutinesandbox.interactors.GetMovieSummaries.MovieSummary
-import javax.inject.Inject
 
 @Composable
 fun RowHeader(text: String) {
@@ -40,58 +27,15 @@ fun MoviesRow(
 
         LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
             items(movies, { it.id }) { movie ->
-                MovieCard(
-                    movie, movieCardFactory, favoriteActionHandler
+                movieCardFactory.MovieCard(
+                    movie, favoriteActionHandler
                 )
             }
         }
     }
 }
 
-@ActivityScoped
-data class MovieCardFactory @Inject constructor(
-    val favoriteButtonfactory: FavoriteButtonFactory,
-    val downloadIndicatorFactory: DownloadIndicatorFactory
-)
 
-@Composable
-private fun MovieCard(
-    movie: MovieSummary,
-    movieCardFactory: MovieCardFactory,
-    favoriteActionHandler: (String, Boolean) -> Unit
-) {
-    Card {
-        Box(
-            Modifier
-                .width(140.dp)
-                .height(240.dp)
-        ) {
-            val imageLoaded = remember { mutableStateOf(false) }
-            Image(
-                rememberImagePainter(data = movie.posterUrl,
-                    builder = {
-                        scale(Scale.FILL) // This doesn't work as expected
-                            .listener(onSuccess = { _, _ -> imageLoaded.value = true })
-                    }), movie.title
-            )
-
-            // Only show the text title if we aren't showing the poster
-            if (!imageLoaded.value) {
-                Text(
-                    text = movie.title,
-                    color = MaterialTheme.colors.primary
-                )
-            }
-
-            movieCardFactory.favoriteButtonfactory.FavoriteButton(
-                movie.id,
-                favoriteActionHandler,
-                Modifier.align(Alignment.BottomEnd)
-            )
-            movieCardFactory.downloadIndicatorFactory.DownloadIndicator(movie.id)
-        }
-    }
-}
 
 
 
