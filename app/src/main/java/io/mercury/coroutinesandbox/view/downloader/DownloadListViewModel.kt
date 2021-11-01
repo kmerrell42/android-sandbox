c0c02754775c1d35c5af707845e7d768a0e711bd
Kelly Merrell <kmerrell@mercuryintermedia.com>
2021-11-01 12:02:03 -0500

    Moving the download button logic out of the activity

--- app/src/main/java/io/mercury/coroutinesandbox/view/downloader/DownloadListViewModel.kt
+++ app/src/main/java/io/mercury/coroutinesandbox/view/downloader/DownloadListViewModel.kt
@@ -3,14 +3,18 @@ package io.mercury.coroutinesandbox.view.downloader
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.mercury.coroutinesandbox.interactors.DownloadMovie
 import io.mercury.coroutinesandbox.interactors.GetDownloadedMovies
 import io.mercury.coroutinesandbox.interactors.GetInProgressDownloads
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 @HiltViewModel
 class DownloadListViewModel @Inject constructor(
     getDownloadedMovies: GetDownloadedMovies,
-    getInProgressDownloads: GetInProgressDownloads
+    getInProgressDownloads: GetInProgressDownloads,
+    downloadMovie: DownloadMovie
 ) : ViewModel() {
     val feature = DownloadListFeature(viewModelScope, getInProgressDownloads, getDownloadedMovies)
+    val downloaderFeature = CheeseyDownloaderFeature(viewModelScope, Dispatchers.IO, downloadMovie)
 }
